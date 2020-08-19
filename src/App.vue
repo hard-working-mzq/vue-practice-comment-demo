156d87fe22c88d42380d809be02e9110c5565ff3
hard-working-mzq <1348223133@qq.com>
2020-08-19 13:03:25 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <header class="site-header jumbotron">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <h1>请发表对Vue的评论</h1>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="container">
+    <Add :addComment="addComment"/>
+    <List :comments="comments" :deleteComment="deleteComment"/>
+  </div>
+  </div>
+</template>
+
+<script>
+  import Add from './components/Add.vue'
+  import List from './components/List.vue'
+
+export default {
+
+  data() {
+    return {
+      comments: [ //数据在哪个组件，更新数据的行为（方法）就应该定义在哪个组件
+        {
+          name: 'AAA',
+          content: 'Vue可以'
+        },
+        {
+          name: 'BBB',
+          content: 'Vue不好'
+        },
+        {
+          name: 'CCC',
+          content: '我喜欢Vue'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    //添加评论
+    addComment (comment) {
+      this.comments.unshift(comment)
+    },
+
+    //删除指定下标的评论
+   deleteComment (index) {
+    this.comments.splice(index,1)
+    }
+  },
+
+  components: {
+    Add,
+    List
+  }
+}
+</script>
